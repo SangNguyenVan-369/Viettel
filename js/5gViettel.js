5cbc29f63454287584368eb313237b64ca571207
SangNguyenVan-369 <nguyenvansang260704@gmail.com>
2024-11-11 22:45:21 +0700

    sang commit

--- js/5gViettel.js
+++ js/5gViettel.js
@@ -1,12 +1,37 @@
 app.controller("5gViettelController", function($scope) {
     $scope.internetPackages = [
-        { title: "MI7D", price: 7000, details: "7.000đ có 700MB Data" },
-        { title: "6M10_100M", price: 60000, details: "60K/180 ngày có 700MB Data" },
-        { title: "ST70K", price: 70000, details: "15GB (500MB/ngày) + Miễn phí..." },
-        { title: "SD90", price: 90000, details: "90K/30 ngày có 45GB..." },
-        { title: "SD120", price: 120000, details: "120K/30 ngày có..." },
-        { title: "ST120K", price: 120000, details: "2GB/ngày + Miễn phí DV..." },
-        { title: "SD150", price: 150000, details: "150K/30 ngày có..." },
-        { title: "5G150", price: 150000, details: "150K/30 ngày có..." }
+        {   
+            title: "MI7D", 
+            price: 7000, 
+            data: 700,
+            unit: "MB",
+            date: "24h",
+            details: "Hết lưu lượng hệ thống dừng truy cập.",
+        },
+        { 
+            title: "6M10_100M", 
+            price: 60000, 
+            data: 100,
+            unit: "MB",
+            date: "30 ngày",
+            details: "Hết lưu lượng hệ thống dừng truy cập.",
+
+        },
+        { 
+            title: "ST70K", 
+            price: 70000, 
+            data: 500,
+            unit: "MB",
+            date: "30 ngày",
+            details: "Hết lưu lượng hệ thống dừng truy cập.",
+        },
+        { 
+            title: "SD90", 
+            price: 90000, 
+            data: 1.5,
+            unit: "GB",
+            date: "30 ngày",
+            details: "Hết lưu lượng hệ thống dừng truy cập.",
+        },
     ];
 });
